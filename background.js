e0c2cf54fa1d705a75b5b2e16f3f024d2edfefcc
Geresia <lsj020618@gmail.com>
2026-07-14 11:46:21 +0900

    Remove Google account auto-selection on OAuth

--- background.js
+++ background.js
@@ -11,14 +11,3 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     files: ['tera_login.js'],
   }).catch(() => {});
 });
-
-// Auto account chooser: inject google_account.js on Google OAuth for Traveloka
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status !== 'complete') return;
-  if (!tab.url?.startsWith('https://accounts.google.com/')) return;
-  if (!tab.url.includes('traveloka')) return;
-  chrome.scripting.executeScript({
-    target: { tabId },
-    files: ['google_account.js'],
-  }).catch(() => {});
-});
